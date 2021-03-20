2e0b31243e695889aa19bc1a210f15b8395dd318
iiFDCT <conor.turner11@gmail.com>
2021-03-20 19:26:40 +0000

    Add Message#guild

--- index.d.ts
+++ index.d.ts
@@ -2224,6 +2224,7 @@ declare namespace Eris {
     application?: MessageApplication;
     attachments: Attachment[];
     author: User;
+    guild: Guild;
     channel: T;
     channelMentions: string[];
     /** @deprecated */
